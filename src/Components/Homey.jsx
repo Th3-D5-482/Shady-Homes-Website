d88d5b6a0181d6f52e304ec76a770acd843b74a5
Megatron482 <davesquare482@gmail.com>
2025-04-01 21:50:43 +0530

    Th3_D5_482

--- src/Components/Homey.jsx
+++ src/Components/Homey.jsx
@@ -3,6 +3,28 @@ import homey from '../assets/homey.png'
 import right_arrow from '../assets/right_arrow.png'
 
 const Homey = () => {
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "d56f8ef5-ac52-4484-9fce-40b975ee85bc");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+        }
+      };    
   return (
     <div className='w-auto mx-8 h-130 mb-20 flex'>
         <div className='w-1/2'>
@@ -13,8 +35,8 @@ const Homey = () => {
             <p className='text-gray-400 mb-8'>We help people not only to make the room at home better, but we help people to feel a more homey and comfortable room, you don't have to worry about the result because of these interiors are made by people who are professionals in their fields with an elegant and lucurious style and with premium quality materials.</p>
             <p className='text-gray-400 mb-8'>Contact us, if your are interested in working with us.</p>
             <div className='border border-[#F79489] w-80 h-15 rounded-full flex items-center px-4'>
-                <input type="email" className ='w-80 h-15 outline-none text-[#F79489]'  placeholder ='Insert your email address' name="" id="" />
-                <div className='w-15 h-12 rounded-full bg-[#FADCD9] flex items-center justify-center'>
+                <input type="email" className ='w-80 h-15 outline-none text-[#F79489]'  placeholder ='Insert your email address' required name="" id="" />
+                <div className='w-15 h-12 rounded-full bg-[#FADCD9] flex items-center justify-center' onClick={onSubmit}>
                     <img src={right_arrow} alt="" />
                 </div>
             </div>
